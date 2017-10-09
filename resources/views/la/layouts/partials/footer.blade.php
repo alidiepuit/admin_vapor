9636bcc15ea35160464c78679361408bbacbb5dd
Diep Luu <lvdiep@fossil.com>
2017-10-09 21:12:22 +0700

    admin vapor

---
+++ resources/views/la/layouts/partials/footer.blade.php
@@ -0,0 +1,8 @@
+@if(!isset($no_padding))
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        Powered by <a target="_blank" href="http://aliteam568.com">AliTeam568.com</a>
+    </div>
+    <strong>Copyright &copy; 2017
+</footer>
+@endif
